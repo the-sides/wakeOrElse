2fa0dde7886d4e413c1fd7057b0bbd5d1b40fec8
the-sides <sides798@gmail.com>
2020-04-12 19:13:18 -0400

    tweet at time

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const env = require('dotenv').config()
 const Twitter = require('twitter')
-console.log(process.env.apiKey)
+const alarm = require('alarm')
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -9,10 +9,19 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-client.post('statuses/update', {
-  status: 'Working on a Twitter bot to wake me up in the morning. It will post embarrassing tweets at a particular time unless I wake up to stop it. I am apologizing in advance... Hopefully, this will not be a problem...'},  
-  function(error, tweet, response) {
-    if(error) throw error;
-    console.log(tweet);  // Tweet body.
-    console.log(response);  // Raw response object.
-});
+const tweet = msg => {
+  client.post(
+    'statuses/update', 
+    { status: msg },  
+    function(error, tweet, response) {
+      if(error) throw error;
+      console.log(tweet);  // Tweet body.
+      console.log(response);  // Raw response object.
+  });
+}
+
+const time = new Date('April 12, 2020 19:15:00')
+
+alarm(time, function(){
+  tweet("If you're seeing this, then it means I went against my mother's concerns, made the bot and it works. No one is safe...")
+})
